53fc8e82a45b656e45b9134bcc93f93a2af4f75c
Dmytro Poltavchenko <dmytro.poltavchenko@bystrov.agency>
2024-09-09 18:53:25 +0200

    refactor: Add 'like' property to employee data and implement like functionality in employees-list-item component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,9 +8,9 @@ import './app.css';
 
 function App() {
   const data = [
-    { name: 'John C.', salary: 800, increase: false, id: 1 },
-    { name: 'Alex M.', salary: 3000, increase: true, id: 2 },
-    { name: 'Carl W.', salary: 5000, increase: false, id: 3 },
+    { name: 'John C.', salary: 800, increase: false, id: 1, like: false },
+    { name: 'Alex M.', salary: 3000, increase: true, id: 2, like: false },
+    { name: 'Carl W.', salary: 5000, increase: false, id: 3, like: false },
   ];
 
   return (
--- src/components/employees-list-item/employees-list-item.jsx
+++ src/components/employees-list-item/employees-list-item.jsx
@@ -1,39 +1,75 @@
+import { Component } from 'react';
 import './employees-list-item.css';
 
-const EmployeesListItem = ({ name, salary, increase }) => {
-  let classNames = 'list-group-item d-flex justify-content-between';
-  if (increase) {
-    classNames += ' increase';
+class EmployeesListItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      increase: false,
+      like: false,
+    };
   }
 
-  return (
-    <li className={classNames}>
-      <span className="list-group-item-label">{name}</span>
-      <input
-        type="text"
-        className="list-group-item-input"
-        defaultValue={salary + '$'}
-      />
-      <div className="d-flex justify-content-center align-items-center">
-        <button
-          type="button"
-          className="btn-cookie btn-sm"
-          aria-label="cookie"
-        >
-          <i className="fas fa-cookie"></i>
-        </button>
+  onIncrease = () => {
+    this.setState(({ increase }) => ({
+      increase: !increase,
+    }));
+  };
 
-        <button
-          type="button"
-          className="btn-trash btn-sm"
-          aria-label="trash"
+  onLike = () => {
+    this.setState(({ like }) => ({
+      like: !like,
+    }));
+  };
+
+  render() {
+    const { name, salary } = this.props;
+    const { increase, like } = this.state;
+
+    let classNames = 'list-group-item d-flex justify-content-between';
+    if (increase) {
+      classNames += ' increase';
+    }
+    if (like) {
+      classNames += ' like';
+    }
+
+    return (
+      <li className={classNames}>
+        <span
+          className="list-group-item-label"
+          onClick={this.onLike}
         >
-          <i className="fas fa-trash"></i>
-        </button>
-        <i className="fas fa-star"></i>
-      </div>
-    </li>
-  );
-};
+          {name}
+        </span>
+        <input
+          type="text"
+          className="list-group-item-input"
+          defaultValue={salary + '$'}
+        />
+        <div className="d-flex justify-content-center align-items-center">
+          <button
+            type="button"
+            className="btn-cookie btn-sm"
+            onClick={this.onIncrease}
+            aria-label="cookie"
+          >
+            <i className="fas fa-cookie"></i>
+          </button>
+
+          <button
+            type="button"
+            className="btn-trash btn-sm"
+            aria-label="trash"
+          >
+            <i className="fas fa-trash"></i>
+          </button>
+          <i className="fas fa-star"></i>
+        </div>
+      </li>
+    );
+  }
+}
 
 export default EmployeesListItem;
